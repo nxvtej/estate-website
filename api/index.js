898f0d19fde8ce9c42506383ca94a785c4ec3775
Navdeep Singh Sehrawat <nxvtej@outlook.com>
2024-02-24 15:53:38 +0530

    connect to mongodb database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,14 @@
 // import { Express } from "express-serve-static-core";
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('connected to mongodb');
+}).catch((err) => {
+    console.log('err');
+});
 const app = express();
 
 app.listen(3000, ()=> {
